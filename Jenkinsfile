8be785640e6c136d88f12b09c91d7d8d4fe9d7be
Bharath KKB <bharathkrishnakb@gmail.com>
2019-02-19 15:57:17 -0800

    Add jenkins file test (#17)
    Added Jenkins CI
    
    * add test file
    
    * update req
    
    * added threaded flask instances
    
    * increase retry
    
    * use requests json func
    
    * separate tests

---
+++ Jenkinsfile
@@ -0,0 +1,51 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building'
+                sh """
+                export BUILD_ID=dontKillMe
+                python3 --version
+                cd asl-api
+                python3 -m virtualenv env
+                ls
+                . env/bin/activate
+                pip install -r requirements.txt
+                pytest -q test_api.py --url=http://0.0.0.0:5000  --local=0 -vv -s
+
+                """
+
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying'
+            }
+        }
+    }
+    post {
+        always {
+            echo 'This will always run'
+        }
+        success {
+            echo 'This will run only if successful'
+        }
+        failure {
+            echo 'This will run only if failed'
+        }
+        unstable {
+            echo 'This will run only if the run was marked as unstable'
+        }
+        changed {
+            echo 'This will run only if the state of the Pipeline has changed'
+            echo 'For example, if the Pipeline was previously failing but is now successful'
+        }
+    }
+}
---
+++ asl-api/test_api.py
@@ -0,0 +1,103 @@
+
+from swagger_spec_validator import validate_spec_url
+from server import createApp, createAppThread
+import flex
+import requests
+from flex.core import load, validate_api_call
+import pprint
+import pytest
+import json
+import sys
+import time
+import threading
+from requests.exceptions import ConnectionError
+"""
+**************************************
+Setup
+**************************************
+"""
+def appThread():
+    app = createAppThread()
+    app.run(host='0.0.0.0', port=5000, debug=False)
+
+
+def test_start_daemon_api_thread(local):
+    print(local)
+    if(local == "0"):
+        print("Starting daemon serve thread")
+        apiThread = threading.Thread(name='Web App', target=appThread)
+        apiThread.setDaemon(True)
+        apiThread.start()
+
+        while not apiThread.is_alive():
+            pass
+    else:
+        print(
+            "Not starting daemon serve thread, assuming server running in a seperate process")
+
+
+def test_thread(url):
+    maxTry = 300
+    currentTry = 0
+    while currentTry < maxTry:
+        currentTry += 1
+        try:
+            testAPIBasePath = "{}/test/api".format(url)
+            response = requests.get(testAPIBasePath + '/hello', timeout=300)
+            if(response.status_code == 200):
+                break
+        except ConnectionError as ex:
+            pass
+"""
+**************************************
+Swagger Infra Tests
+**************************************
+"""
+def validateSwagger(url):
+    testAPIBasePath = "{}/test/api".format(url)
+    validate_spec_url(testAPIBasePath + '/swagger.json')
+
+# check connection to server
+
+
+def test_case_connection(url):
+    testAPIBasePath = "{}/test/api".format(url)
+    response = requests.get(testAPIBasePath + '/hello')
+    assert response.status_code == 200
+
+# pytest for validating swagger schema
+
+
+def test_validateSwagger(url):
+    assert validateSwagger(url) == None
+
+# helper for validate hello schema
+
+
+def hello_schema(url, schema):
+    testAPIBasePath = "{}/test/api".format(url)
+    response = requests.get(testAPIBasePath + '/hello')
+    validate_api_call(schema, raw_request=response.request,
+                      raw_response=response)
+
+# pytest for validating hello schema
+
+
+def test_hello_schema(url):
+    testAPIBasePath = "{}/test/api".format(url)
+    schema = flex.load(testAPIBasePath + '/swagger.json')
+    assert hello_schema(url, schema) == None
+
+# pytest for validating data returned by /hello endpoint
+
+
+def test_hello_data(url):
+    testAPIBasePath = "{}/test/api".format(url)
+    response = requests.get(testAPIBasePath + '/hello')
+    data = response.json()
+    print(data)
+    assert data["hello"] == "hello"
+
+# # this is for debugging individual tests
+# # if __name__ == "__main__":
+# #     test_hello_data()
