d90fd67214528d3b6d873039696a5315ceb52bb8
Kushal Cuttari <Kushal@Kushals-MBP.attlocal.net>
2019-03-23 15:02:40 -0700

    Updated predictor script to make it queryable

--- image-classifier/predictor_script.py
+++ image-classifier/predictor_script.py
@@ -2,20 +2,12 @@ from keras.models import load_model
 from keras import optimizers
 import cv2
 import numpy as np
-import argparse
-
-#Call is as follows python3 predictor_script.py './path_to_image/img_name.jpg
-
 model = load_model('model_keras.h5')
 model.load_weights('model_weights.h5')
 model.compile(loss='categorical_crossentropy',
               optimizer=optimizers.RMSprop(lr=1e-4), metrics=['acc'])
 
-parser = argparse.ArgumentParser(description='Get Image Location')
-parser.add_argument("img_location")
-args = parser.parse_args()
-
-img = cv2.imread(args.img_location)
+img = cv2.imread('Y_test.jpg')
 img = cv2.resize(img, (200, 200))
 img = np.reshape(img, [1, 200, 200, 3])
 classes = model.predict_classes(img)
