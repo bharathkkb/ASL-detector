ba424ac88943ad6db5a74b5008ce98fdb137b3df
Kushal Cuttari <Kushal@Kushals-MacBook-Pro.local>
2019-04-06 17:02:05 -0700

    Equalization of histogram on rgb scale

--- image-classifier/process_image.py
+++ image-classifier/process_image.py
@@ -1,20 +1,11 @@
-import numpy as np
 import cv2
+import numpy as np
 
-cap = cv2.VideoCapture(0)
+def equalize_histogram(img):
+    img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+    # equalize the histogram of the Y channel
+    img_yuv[:,:,0] = cv2.equalizeHist(img_yuv[:,:,0])
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+    # convert the YUV image back to RGB format
+    img_output = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
+    return img_output
