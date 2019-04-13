84199575a432f6b0bd063fffda21c133c3894c3e
Kushal Cuttari <Kushal@Kushals-MacBook-Pro.local>
2019-04-13 15:10:39 -0700

    Got rid of CV2.img_shows

--- image-classifier/process_image.py
+++ image-classifier/process_image.py
@@ -10,10 +10,7 @@ def equalize_histogram_clahe(img):
 
     img_yuv = cv2.merge(planes)
     img_output = cv2.cvtColor(img_yuv, cv2.COLOR_LAB2BGR)
-
-    cv2.imshow('Color input image', img)
-    cv2.imshow('Histogram equalized', img_output)
-    cv2.waitKey(0)
+    return img_output
 
 def equalize_histogram(img):
     img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
