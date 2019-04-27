f72a44efb2d323d940971a38c04e468fdf46f3ef
vikr01 <vik120@icloud.com>
2019-04-27 03:14:34 -0700

    use environment variable for domain to hit apis

--- frontend/src/helpers/apis.js
+++ frontend/src/helpers/apis.js
@@ -1,11 +1,11 @@
 export const predict = body =>
-  fetch('/test/api/predict', {
+  fetch(`${process.env.DOMAIN || ''}/test/api/predict`, {
     method: 'POST',
     body,
   }).then(async res => (await res.json()).id);
 
 export const getPrediction = body =>
-  fetch('/test/api/job', {
+  fetch(`${process.env.DOMAIN || ''}/test/api/job`, {
     method: 'POST',
     body,
   }).then(res => res.json());
