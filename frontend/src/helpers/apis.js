483b84760a7b9d20ce6124b67be92a5b178b37fd
Bharath KKB <bharathkrishnakb@gmail.com>
2019-04-26 22:34:24 -0700

    hard code ip

--- frontend/src/helpers/apis.js
+++ frontend/src/helpers/apis.js
@@ -1,11 +1,11 @@
 export const predict = body =>
-  fetch('http://localhost:5000/test/api/predict', {
+  fetch('http://34.74.97.21:5000/test/api/predict', {
     method: 'POST',
     body,
   }).then(async res => (await res.json()).id);
 
 export const getPrediction = body =>
-  fetch('http://localhost:5000/test/api/job', {
+  fetch('http://34.74.97.21:5000/test/api/job', {
     method: 'POST',
     body,
   }).then(res => res.json());
