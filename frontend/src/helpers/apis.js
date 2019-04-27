aaf721193c6ba9d4f2d6abd72ffe8d463ebae084
Bharath KKB <bharathkrishnakb@gmail.com>
2019-04-26 23:43:29 -0700

    Add production build (#73)
    * fix url
    
    * added prd files
    
    * hard code ip

--- frontend/src/helpers/apis.js
+++ frontend/src/helpers/apis.js
@@ -1,11 +1,11 @@
 export const predict = body =>
-  fetch('/test/api/predict', {
+  fetch('http://34.74.97.21:5000/test/api/predict', {
     method: 'POST',
     body,
   }).then(async res => (await res.json()).id);
 
 export const getPrediction = body =>
-  fetch('/test/api/job', {
+  fetch('http://34.74.97.21:5000/test/api/job', {
     method: 'POST',
     body,
   }).then(res => res.json());
