72fe5c4966cd157ec33c43dda5993c812e072a6f
Vikram Rangaraj <vik120@icloud.com>
2019-04-28 00:29:17 -0700

    fix issue with environment variable not loading (#76)

--- frontend/src/helpers/apis.js
+++ frontend/src/helpers/apis.js
@@ -1,11 +1,11 @@
 export const predict = body =>
-  fetch(`${process.env.DOMAIN || ''}/test/api/predict`, {
+  fetch(`${process.env.REACT_APP_DOMAIN || ''}/test/api/predict`, {
     method: 'POST',
     body,
   }).then(async res => (await res.json()).id);
 
 export const getPrediction = body =>
-  fetch(`${process.env.DOMAIN || ''}/test/api/job`, {
+  fetch(`${process.env.REACT_APP_DOMAIN || ''}/test/api/job`, {
     method: 'POST',
     body,
   }).then(res => res.json());
