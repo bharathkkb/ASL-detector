ad2e28be37dbdc2b6f4cb680598ca908339b889a
Vikram Rangaraj <vik120@icloud.com>
2019-04-27 18:46:14 -0700

    Use environment variable for API domain (#75)
    * initial frontend with requests
    
    * add netlify config
    
    * use environment variable for domain to hit apis

--- frontend/src/helpers/apis.js
+++ frontend/src/helpers/apis.js
@@ -1,11 +1,11 @@
 export const predict = body =>
-  fetch('http://34.74.97.21:5000/test/api/predict', {
+  fetch(`${process.env.DOMAIN || ''}/test/api/predict`, {
     method: 'POST',
     body,
   }).then(async res => (await res.json()).id);
 
 export const getPrediction = body =>
-  fetch('http://34.74.97.21:5000/test/api/job', {
+  fetch(`${process.env.DOMAIN || ''}/test/api/job`, {
     method: 'POST',
     body,
   }).then(res => res.json());
