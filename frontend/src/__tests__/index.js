28c2e34596db2d6e0d4ae8e01dc7fed83413cea7
vikr01 <vik120@icloud.com>
2019-05-05 00:06:55 -0700

    remove sample test

--- frontend/src/__tests__/index.js
+++ frontend/src/__tests__/index.js
@@ -5,12 +5,6 @@ const A_TEST_FILEPATH = require.resolve('../../../asl-api/A_test.jpg');
 const DOMAIN = process.env.FRONTEND_DOMAIN;
 console.log('domain is ', DOMAIN);
 
-describe('sample test', () => {
-  it('works', () => {
-    expect(1).toBe(1);
-  });
-});
-
 describe('image upload', () => {
   let browser;
   let page;
