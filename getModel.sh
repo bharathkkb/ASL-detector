df872bd2e50b51cc9509a0f3e898d8aacfb139b6
Bharath KKB <bharathkrishnakb@gmail.com>
2019-05-05 20:18:19 -0700

    try using a newer model

--- getModel.sh
+++ getModel.sh
@@ -1,1 +1,1 @@
-curl --url https://storage.googleapis.com/asl-models/model_keras.h5 --output ./asl-api/model_keras.h5
+curl --url https://storage.googleapis.com/asl-models/model_keras_new_2.h5 --output ./asl-api/model_keras.h5
